ce670ecd2beae44bbe3565776557448803607d55
Tonatiuh Martinez <merodeador.nocturno@gmail.com>
2018-06-01 00:25:57 -0500

    Added shape shifting capabilities

--- js/composition_lambda.js
+++ js/composition_lambda.js
@@ -58,13 +58,37 @@ const canUpdatePosition = (state) => ({
   }
 });
 
-const returnsCanvasById = (canvasContainer) => document.getElementById(canvasContainer);
+const returnsCanvasById = (canvasContainer) => document.getElementById(canvasContainer).getContext('2d');
+
+const definesShape = (state) => ({
+    defineSquare: () => {
+        const canvasContext = returnsCanvasById(state.canvasContainer);
+        canvasContext.fillStyle = state.color;
+        canvasContext.fillRect(state.x, state.y, state.side, state.side);
+    },
+    defineCircle:() => {
+        const canvasContext = returnsCanvasById(state.canvasContainer);
+        canvasContext.fillStyle = state.color;
+        canvasContext.beginPath();
+        canvasContext.arc(state.x, state.y, state.side / 2, 0, 2 * Math.PI);
+        canvasContext.stroke();
+        canvasContext.fill();
+    }
+});
+
+const hasShape = (state) => ({
+    setShape: (shape) => {
+        state.shape = shape;
+    }
+});
 
 const canBeDrawn = (state) => ({
   draw: () => {
-    const canvasContext = returnsCanvasById(state.canvasContainer).getContext('2d');
-    canvasContext.fillStyle = state.color;
-    canvasContext.fillRect(state.x, state.y, state.side, state.side);
+    const typesOfShape = {
+        square: state.defineSquare,
+        circle: state.defineCircle,
+    };
+    typesOfShape[state.shape]();
   }
 });
 
@@ -76,13 +100,14 @@ const canBeHidden = (state) => ({
 
 const isStringifyable = (state) => ({
   toString: () => {
-    let txt = '\n';
-    for (let i in this) {
-      if (this.hasOwnProperty(i) && typeof this[i] !== 'function') {
-        txt += `\t${i}: ${this[i]}\n`;
-      }
-    }
-    return `${txt}\n`;
+    const map = new Map();
+    const stateKeys = Object.keys(state)
+      .filter(key => typeof state[key] !== 'function');
+    stateKeys.forEach(key => {
+      map.set(key, state[key]);
+    });
+
+    return map.entries();
   }
 });
 
@@ -92,6 +117,7 @@ const createSquare = (x = 10, y = 20, side = 50, color = 'red') => {
     y,
     side,
     color,
+    shape: 'square',
   };
   
   return Object.assign(
@@ -99,6 +125,12 @@ const createSquare = (x = 10, y = 20, side = 50, color = 'red') => {
     isStringifyable(state),
     canSetColor(state)
   );
+};
+
+const createCircle = (x, y, radius, color) => {
+    const state = {
+
+    };
 };
 
 const animateShape = (shape, vx = 0.1, vy = 0.09, xLimit = 450, yLimit = 200, deltaTime = 0, canvasContainer = 'myCanvas') => {
@@ -114,6 +146,8 @@ const animateShape = (shape, vx = 0.1, vy = 0.09, xLimit = 450, yLimit = 200, de
   return Object.assign(
     state,
     shape,
+    definesShape(state),
+    hasShape(state),
     canMove(state),
     canSpeed(state),
     canStopAtBoundaries(state),
@@ -125,9 +159,13 @@ const animateShape = (shape, vx = 0.1, vy = 0.09, xLimit = 450, yLimit = 200, de
 };
 
 const square = createSquare();
+
 const movingSquare = animateShape(square);
 const movingSquare2 = animateShape(square, 0.05, 0.15);
 movingSquare2.color = 'blue';
+
+const movingSquare3 = animateShape(square, 0.08, 0.07);
+movingSquare3.color = 'maroon';
 
 /* game constants */
 const getMaxFPS = () => 60;
@@ -149,13 +187,16 @@ const gameLoop = (timestamp) => {
     movingSquare.updateState();
     movingSquare2.setDeltaTime(getTimeStep());
     movingSquare2.updateState();
+    movingSquare3.setDeltaTime(getTimeStep());
+    movingSquare3.updateState();
 
     delta -= getTimeStep();
   }
 
-  returnsCanvasById('myCanvas').getContext('2d').clearRect(0, 0, 500, 250);
+  returnsCanvasById('myCanvas').clearRect(0, 0, 500, 250);
   movingSquare.draw();
   movingSquare2.draw();
+  movingSquare3.draw();
   requestAnimationFrame(gameLoop);
 };
 
